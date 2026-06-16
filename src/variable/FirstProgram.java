e825284cde154c25436ec14345df8ac2f10aba21
rajesh <rajeshducat988@gmail.com>
2026-06-16 15:36:57 +0530

    4tht commit

--- src/variable/FirstProgram.java
+++ src/variable/FirstProgram.java
@@ -6,7 +6,13 @@ public class FirstProgram{
     public static void main(String[] args) {
 
         System.out.println("hello started cicd");
+        System.out.println("8888888888888");
 
+    }
+
+    public void test(){
+
+        System.out.println("this is test method");
     }
 
 }
